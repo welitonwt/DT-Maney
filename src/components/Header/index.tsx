57d8a67bf0c3be81fd3d331868d87f21febe5bbf
weliton <weliton.ww@hotmail.com>
2023-10-17 17:57:25 -0300

    styled background start

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import { HeaderContainer, HeaderContent } from './styled'
+
+import logo from '../../assets/logo.svg'
+
 export function Header() {
-  return <h1>helo yord</h1>
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} alt="Logotipo do Ignite" />
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
